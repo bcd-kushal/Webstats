bd14b7a128163f40934c17b44e74fe61cad3f9d1
Kushal <dev@kushalkumarsaha.com>
2024-03-27 02:00:15 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import { maxContentSpace } from "@/decorators/universals"
+import { SiteList } from "@/components/siteList/SiteList"
+import { StatsContainer } from "@/components/mainData/statsContainer/StatsContainer"
 
-export default function Home() {
-  return (
-    <></>
-  )    
+export default function HomePage() {
+	return (
+		<div className={`px-4 md:px-8 ${maxContentSpace} overflow-hidden flex flex-col gap-5 md:pt-4 md:gap-6 md:flex-row items-stretch justify-start md:justify-stretch w-full h-full relative`} >
+			<SiteList/>
+			<StatsContainer title="" dataAttrVal="" highlightsData={{}} monthlyViews={[]} link=""/>
+		</div>
+	)
 }
